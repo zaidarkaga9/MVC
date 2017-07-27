e71be86befefd84b051aba1596fe2091d62868d5
zaidarkaga9 <zaidarkaga8@gmail.com>
2017-07-27 15:04:40 +0300

    Update Database.php

--- app/lib/Database.php
+++ app/lib/Database.php
@@ -161,34 +161,6 @@ class L_Database
             }
     }
 
-    public function get_xls($sql,$filename,$columns)
-    {
-            $this->conn->query("SET NAMES 'utf8'");
-		    $this->conn->query("SET CHARACTER SET utf8");
-            $stmt = $this->conn->prepare($sql);
-            $stmt->execute();
-            $filename = "public/file/".$filename.".xls";
-            $data = fopen($filename, 'w');
-            fputcsv($data,$columns, "\t", '"');
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                fputcsv($data, $row, "\t", '"');
-            }
-            fclose($data);
-            if (file_exists($filename)) 
-            {
-                header('Content-Description: File Transfer');
-                header('Content-Type: application/octet-stream');
-                header('Content-Disposition: attachment; filename="'.basename($filename).'"');
-                header('Expires: 0');
-                header('Cache-Control: must-revalidate');
-                header('Pragma: public');
-                header('Content-Length: ' . filesize($filename));
-                readfile($filename);
-                unlink($filename);
-                exit;
-            }
-    }
-
     public function get_json_file($sql,$filename)
     {
             $this->conn->query("SET NAMES 'utf8'");
